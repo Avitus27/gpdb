9165801b6300b64d4e113011f89eeeb5fbe73262
Mervyn Galvin <mervyn511@gmail.com>
2018-06-12 13:11:53 +0100

    fix path error, load DB

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -2,5 +2,9 @@
 	require_once(__DIR__."/../vendor/autoload.php"); // Loads anything that's been added via composer
 	$dotenv = new Dotenv\Dotenv(__DIR__."/../"); // for .env
 	$dotenv->load();
-
+	$dotenv->required(['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_BASE']);
+	$host = $_ENV['DB_HOST']; $database = $_ENV['DB_BASE'];
+	$user = $_ENV['DB_USER']; $pass = $_ENV['DB_PASS'];
+	$db = new MysqliDb($host, $user, $pass, $database);
+	$db->setPrefix('gpdb_');
 ?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__.'../bootstrap/autoload.php';
+    require_once __DIR__.'/../bootstrap/autoload.php';
 
 
 
